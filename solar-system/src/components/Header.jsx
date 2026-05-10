160d161bccdd59a59faf7b088a180bbb3f46d252
Ademicope <michaeladekunle00@gmail.com>
2026-05-10 23:16:14 +0100

    added site index and style files; added header and footer components

---
+++ solar-system/src/components/Header.jsx
@@ -0,0 +1,28 @@
+
+function Header() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src="https://res.cloudinary.com/dfen3riqc/image/upload/v1769872116/planet-logo_i5a4ek.png" />
+                </nav>
+                <div class="header-detail">
+                    <div class="header-left">
+                        <h1>Explore Our Solar System Through Data</h1>
+                        <p>Understand the planets not just by name, but by measurable facts. From size and mass to gravity and
+                            density, this page breaks down the solar system in a clear, data-driven way.</p>
+                        <div class="header-buttons">
+                            <button>Explore the Data</button>
+                            <button id="contact-button">Contact us</button>
+                        </div>
+                    </div>
+                    <div class="header-img">
+                        <img src="https://res.cloudinary.com/dfen3riqc/image/upload/v1769872960/blue-planet_pgns10.png" alt="" />
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
