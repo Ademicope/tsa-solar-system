d707b5aa324ada70500f43918dcf582eacad5894
Ademicope <michaeladekunle00@gmail.com>
2026-05-13 23:44:36 +0100

    fetched planets details from api

--- solar-system/src/components/Footer.jsx
+++ solar-system/src/components/Footer.jsx
@@ -2,7 +2,20 @@
 
 function Footer() {
     return (
-        <></>
+        <>
+            <footer>
+                <h3>About</h3>
+                <p>first name of group members</p>
+                <hr />
+                <div className="terms">
+                    <p>&copy; 2026 Design by Amaka & Ifeoma A.</p>
+                    <div>
+                        <p>Built by <a href="https://github.com/Ademicope/tsa-solar-system/tree/main/solar-system">Group 1.</a>All rights reserved</p>
+                        <a href="https://tsacademyonline.com/">TSacademy</a>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
 
